458e2870f2ebde937ba2a21450719fa0e3f19ddb
BuildTools <unconfigured@null.spigotmc.org>
2018-09-08 18:06:11 -0400

    Set up state machine

---
+++ FlappyBirdClone/State.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Sonar
+{
+	class State
+	{
+	public:
+		virtual void Init() = 0;
+		virtual void HandleInput() = 0;
+		virtual void Update() = 0;
+		//deals with time difference between frames
+		virtual void Draw(float delta) = 0;
+
+		virtual void Pause() {}
+		virtual void Resume() {}
+	};
+}
