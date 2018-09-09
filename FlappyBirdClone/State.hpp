4671887f58c0c23d90ef3495b306d10d5ebaef95
BuildTools <unconfigured@null.spigotmc.org>
2018-09-08 20:13:37 -0400

    added Game State

--- FlappyBirdClone/State.hpp
+++ FlappyBirdClone/State.hpp
@@ -7,7 +7,7 @@ namespace Sonar
 	public:
 		virtual void Init() = 0;
 		virtual void HandleInput() = 0;
-		virtual void Update() = 0;
+		virtual void Update(float delta) = 0;
 		//deals with time difference between frames
 		virtual void Draw(float delta) = 0;
 
--- FlappyBirdClone/main.cpp
+++ FlappyBirdClone/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "Game.hpp"
 
 int main()
 {
+	Sonar::Game(1024, 1024, "SFML Starter");
 	return EXIT_SUCCESS;
 }
